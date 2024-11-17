4be73c6fba7e41af2aa04ae1218ab99551bf6858
Ali-Firdaus <firdaus.ali.nita@gmail.com>
2024-11-17 13:04:38 +0000

    updated sicgulf v14.18 for SI-6

---
+++ auto_repair/public/www/chunk-TUSD6HEG.js
@@ -0,0 +1,2 @@
+import{Ib as i,Jb as r,dc as a,ka as o,kc as p}from"./chunk-NKIVXXOO.js";import"./chunk-WPBPOQOO.js";var u=(()=>{let t=class t{constructor(){}ngOnInit(){}};t.\u0275fac=function(n){return new(n||t)},t.\u0275cmp=o({type:t,selectors:[["app-opportunities"]],standalone:!0,features:[p],decls:2,vars:0,template:function(n,c){n&1&&(i(0,"p"),a(1,` opportunities works!
+`),r())}});let e=t;return e})();export{u as OpportunitiesComponent};
---
+++ auto_repair/public/www/chunk-WLJJ6RQW.js
@@ -0,0 +1,2 @@
+import{Ib as a,Jb as i,dc as r,ka as o,kc as s}from"./chunk-NKIVXXOO.js";import"./chunk-WPBPOQOO.js";var l=(()=>{let e=class e{constructor(){}ngOnInit(){}};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-leads"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(a(0,"p"),r(1,` leads works!
+`),i())}});let t=e;return t})();export{l as LeadsComponent};
