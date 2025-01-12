9a382795a3b9d204406149c177fbd367f9a9e1d0
Ali-Firdaus <firdaus.ali.nita@gmail.com>
2025-01-12 06:57:26 +0000

    updated sicgulf v16.12 for si-6

---
+++ auto_repair/public/www/chunk-GNV6U7S5.js
@@ -0,0 +1,1 @@
+import{c as o,g as t}from"./chunk-76L2T4U6.js";var e=o({providedIn:"root"},t({title:"SI-6",version:"b.0.16.12"}));export{e as a};
--- auto_repair/public/www/chunk-QVGAV56A.js
+++
@@ -1,1 +0,0 @@
-import{c as o,g as t}from"./chunk-76L2T4U6.js";var e=o({providedIn:"root"},t({title:"SI-6",version:"b.0.16.10"}));export{e as a};
