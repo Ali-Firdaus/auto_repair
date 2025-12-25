db06913151d2ac2641a1e0c0b554fc953eac29cc
Ali-Firdaus <firdaus.ali.nita@gmail.com>
2025-12-25 06:48:20 +0000

    revert back to v0.20.30 for si-6 for nginx error

--- auto_repair/public/www/chunk-2G7NLI5B.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./chunk-I6UV4FNW.js";function r(e){return+t(e)}export{r as a};
---
+++ auto_repair/public/www/chunk-PI4S3OQZ.js
@@ -0,0 +1,1 @@
+import{a as e}from"./chunk-OTUPZXRH.js";function a(r,t,d){return e(r,t*7,d)}export{a};
